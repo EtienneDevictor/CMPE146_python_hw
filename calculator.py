590f83bfd0a1b0f039a6080aa36b351085b97c29
Etienne Devictor <etienne.devictor@sjsu.edu>
2021-10-12 16:41:57 -0700

    style- removes call to parse_input in calculator,py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,23 @@
 def calculator(number1, number2, operator):
-	x = float(number1)
-	y = float(number2)
-	if operator == '+':
-		z = x + y
-	elif  operator == '-':
-		z = x - y
-	elif operator == '*':
-		z = x * y
-	elif operator == '/':
-		z = x / y
-	elif operator == '//':
-		z = x // y
-	elif operator == '**':
-		z = pow(x, y)
+	if number1 != float or number2 != float:
+		z = 'One of you numbers is invalid'
 	else:
-		z = 'Invalid Operator'
+		x = float(number1)
+		y = float(number2)
+		if operator == '+':
+			z = x + y
+		elif  operator == '-':
+			z = x - y
+		elif operator == '*':
+			z = x * y
+		elif operator == '/':
+			z = x / y
+		elif operator == '//':
+			z = x // y
+		elif operator == '**':
+			z = pow(x, y)
+		else:
+			z = 'Invalid Operator'
 	print(z)
 
 def parse_input():
@@ -22,4 +25,4 @@ def parse_input():
 	list = i.split()
 	calculator(list[0], list[2], list[1])
 
-parse_input()
+
